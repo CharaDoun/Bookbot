6bcd1b53941507f61b85efc12295c5c908899477
CharaDoun <dounchar@gmail.com>
2024-04-20 22:53:54 +0300

    Added count_words function that counts the number of words inside a book and prints it in the terminal

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 def main():
     with open("books/frankenstein.txt") as frankenstein:
         book_contents = frankenstein.read()
+        count_words(book_contents)
+
+
+def count_words(text):
+    words = text.split()
+    print(len(words))
+
 
 main()
